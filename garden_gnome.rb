8c2cbaafff5c5881e7b3f0b654d287752afcacb0
gracehillen <grahillen19@ahastars.org>
2018-07-30 14:08:08 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -5,10 +5,11 @@ class GardenGnome
   attr_writer age 
   attr_writer gluten_allergy
   attr_reader gluten_allergy
-  initialize (personality, hat_color)
+  initialize (personality, hat_color= red)
      @personality = evil
      @hat_color = hat_color
   end
   attr_reader personality
+  attr_reader hat_color
 
 end
