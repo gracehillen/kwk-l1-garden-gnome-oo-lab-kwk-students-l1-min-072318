9d178f590a378c391707fa15756fb779c4d8b854
gracehillen <grahillen19@ahastars.org>
2018-07-30 14:00:35 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,2 +1,8 @@
 class GardenGnome
-  attr_writer 
+  attr_writer name 
+  attr_reader name
+  attr_reader age 
+  attr_writer age 
+  attr_writer gluten_allergy
+  attr_reader gluten_allergy
+  initialize()
